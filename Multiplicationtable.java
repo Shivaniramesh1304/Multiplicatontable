276abe3c57d8245bf63228b885816fda64bec58f
Shivaniramesh1304 <shivaniramesh1304@gmail.com>
2025-01-30 21:45:49 +0530

    Add files via upload

---
+++ Multiplicationtable.java
@@ -0,0 +1,9 @@
+public class Multiplicationtable {
+    public static void main(String[]args){
+        int number=3;
+        for(int i=1;i<10;i++){
+            System.out.print(number+"x"+i+"="+(number*i));
+        }
+    }
+    
+}
